b4542791fec84567249e8d446894d167c2ea18fc
Roman Datsyshyn <roman.datsyshyn@hotel-spider.com>
2022-04-08 19:35:29 +0300

    added socket.io

---
+++ helpers/formatMessage.js
@@ -0,0 +1,9 @@
+const formatMessage = (username, text) => {
+  return {
+    username,
+    text,
+    time: Date.now(),
+  };
+};
+
+module.exports = formatMessage;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const http = require("http");
 const cors = require("cors");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 const db = require("./database");
@@ -37,6 +38,58 @@ app.use("/serviceSellersPhoto", express.static("serviceSellersPhoto"));
 app.use("/static", express.static("static"));
 
 const server = http.createServer(app);
+const io = socketio(server);
+
+const {
+  getActiveUser,
+  exitRoom,
+  newUser,
+  getIndividualRoomUsers,
+} = require("./helpers/userHelper");
+const formatMessage = require("./helpers/formatMessage");
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ username, room }) => {
+    newUser(socket.id, username, room);
+    socket.join(room);
+
+    socket.emit(
+      "message",
+      formatMessage(username, "Messages are limited to this room! ")
+    );
+
+    socket.broadcast
+      .to(room)
+      .emit(
+        "message",
+        formatMessage(username, `${username} has joined the room`)
+      );
+
+    io.to(room).emit("getUsers", {
+      users: getIndividualRoomUsers(room),
+    });
+  });
+
+  socket.on("chatMessage", ({ username, room, msg }) => {
+    io.to(room).emit("message", formatMessage(username, msg));
+  });
+
+  // socket.on("disconnect", () => {
+  //   const user = exitRoom(socket.id);
+
+  //   if (user) {
+  //     io.to(user.room).emit(
+  //       "message",
+  //       formatMessage("WebCage", `${user.username} has left the room`)
+  //     );
+
+  //     // Current active users and room name
+  //     io.to(user.room).emit("roomUsers", {
+  //       users: getIndividualRoomUsers(user.room),
+  //     });
+  //   }
+  // });
+});
 
 server.listen(process.env.PORT, (err) =>
   console.log(err ? err : `Server listening on port ${process.env.PORT}...`)
